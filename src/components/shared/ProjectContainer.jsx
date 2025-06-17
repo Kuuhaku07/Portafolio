d4935fbc66c332c0198a6b4ab82a7e1cfb3a24a3
Odiseo <uliseslugo07@gmail.com>
2025-06-17 18:42:45 -0400

    bueno una pequeña mejora

--- src/components/shared/ProjectContainer.jsx
+++ src/components/shared/ProjectContainer.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Scrollbar } from 'react-scrollbars-custom';
 import './ProjectContainer.css';
 
 const ProjectContainer = ({ title, children }) => {
   return (
     <section className="project-container">
       <h2 className="project-container-title">{title}</h2>
-      <div className="project-container-scroll">
-        {children}
-      </div>
+      <Scrollbar style={{ width: '100%', height: 760 }} noScrollX={false} noScrollY thumbSize={24}>
+        <div className="project-container-scroll-content">
+          {children}
+        </div>
+      </Scrollbar>
     </section>
   );
 };
